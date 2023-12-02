82ad55714d7f6b373bf5e4b5cd6f20f9f3ec1f30
Stephen-O1 <onyangostephen654@gmail.com>
2023-12-02 02:24:05 -0800

    Code corrections

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,73 +1,70 @@
 #include "lists.h"
 
 /**
- *insert_node- malloc and insert node into sorted singly linked list 
+ * insert_node - malloc and insert node into sorted singly linked list
  * @head: pointer to head of linked list
  * @number: data for new node
  * Return: address of new node, or NULL if failed
-*/
+ */
 
-listint *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-  listint_t *tmp = NULL;
-  listint_t *new = NULL;
+	listint_t *tmp = NULL;
+	listint_t *new = NULL;
 
-  if (!head)
-    return (NULL);
+	if (!head)
+		return (NULL);
 
-  /* malloc new node */
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    return (NULL);
-  new->n = number;
-  new->next = NULL;
+	/* malloc new node */
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
 
-  /* IF NO LINKED LISTS, INSERT NODE as the only member */
-  if (*head == NULL)
-    {
-      *head = new;
-      (*head)->next = NULL;
-      return (new);
-    }
-
-  /*if only one node in linked list, do compariosn and insert */
-  if ((*head)->next == NULL)
-    {
-      if ((*head)->n < new->n)
-	(*head)->next = new;
-      else
+	/* if no linked list, insert node as the only member */
+	if (*head == NULL)
 	{
-	  new->next = *head;
-	  *head = new;
+		*head = new;
+		(*head)->next = NULL;
+		return (new);
 	}
-      retrun (new);
-    }
-
-  /*if lots of nodes in linked list, do comparison and insert*/
-  tmp = *head;
-  while (tmp->next != NULL)
-    {
-      /*if new node num is smalled than first node, insert*/
-      if (new-> < tmp->n)
+	/* if only one node in linked list, do comparision and insert */
+	if ((*head)->next == NULL)
 	{
-	  new->next = tmp;
-	  *head = new;
-	  return (new);
+		if ((*head)->n < new->n)
+			(*head)->next = new;
+		else
+		{
+			new->next = *head;
+			*head = new;
+		}
+		return (new);
 	}
-      /*if new node sum is the same as an existing node, insert*/
-      /* compare previous node and next node, insert in between */
 
-      if (((new->n > tmp->n) && (new-> < (tmp->next)->n)) ||
-	  (new->n == tmp->n))
+	/* if lots of nodes in linked list, do comparision and insert */
+	tmp = *head;
+	while (tmp->next != NULL)
 	{
-	  new->next = tmp->next;
-	  tmp->next = new;
-	  return (new);
+		/* if new node num is smaller than first node, insert */
+		if (new->n < tmp->n)
+		{
+			new->next = tmp;
+			*head = new;
+			return (new);
+		}
+		/* if new node num is the same as an existing node, insert */
+		/* compare previous node and next node, insert in between */
+		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
+		    (new->n == tmp->n))
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
 	}
-      tmp = tmp->next;
-    }
-  /* if new node is greatest and never inserted, insert now */
-  tmp->next = new;
-  return (new);
+	/* if new node is greatest and never inserted, insert now */
+	tmp->next = new;
+	return (new);
 }
-
