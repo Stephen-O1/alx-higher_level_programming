5cf8c815f9e4a7dd225e1ce6f637395b70d4687b
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-04 11:13:34 -0700

    Updated script for showing table details

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- print full description of table; without using 'Describe [tablename]'
---
---Table create Table
+-- Table create Table
 -- first-table CREATE TABLE 'first_table'
 
 SHOW CREATE TABLE first_table;
+
