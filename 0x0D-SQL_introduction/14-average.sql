b4a84bfb16eb429408b949353be41ea123c478ed
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-04 12:16:12 -0700

    Finding average score and saving in avergae colum

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- compute average score in the table
+-- result column name should be average
+-- database name to be passed as anargument of the mysql command
+
+SELECT AVG(score) AS 'average' FROM second_table;
