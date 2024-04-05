703b934338ebfa8cbbe8f5e9d0bc255b8863ffcf
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-04 20:19:05 -0700

    create a table with default vaules

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- create the table id_not_null
+-- data in table: id INT with the default value 1, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+       id INT DEFAULT 1,
+       name VARACHAR(256)
+);
