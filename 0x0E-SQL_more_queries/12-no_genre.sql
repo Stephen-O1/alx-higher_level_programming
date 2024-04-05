ecbcf1dd491c246561e3a1b96db765f285b43824
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-05 05:48:28 -0700

    Updated script

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,9 +2,9 @@
 -- each record should display; tv_shows.title - tv_show_genres.genre_id
 -- result must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-   SELECT tv_shows.titles, tv_show_genres.genre_id
+   SELECT tv_shows.title, tv_show_genres.genre_id
      FROM tv_shows
 LEFT JOIN tv_shows_genres
        ON tv_shows.id = tv_show_genres.show_id
     WHERE tv_show_genres.genre_id is NULL
-ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
+ ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
