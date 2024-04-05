28118128af22c1034ceeedbe3d58210c6885cc37
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-04 20:11:34 -0700

    create a new table with some data

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- create table force_name
+-- data in table: id INT, name VARCHAR(256) can't be null
+
+CREATE TABLE IF NOT EXISTS force_name(
+       id INT,
+       name VARCHAR(256) NOT NULL
+);
