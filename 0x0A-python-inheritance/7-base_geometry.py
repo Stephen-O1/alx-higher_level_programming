084921fe45751109e54bb5acea81dbce75c80b81
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-09 02:33:23 -0700

    instance method area and value validation

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 7-base_geometry
+
+Contains BaseGeometry
+with public instance method area and integer_validation
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+        integer_validator(self, name, value)
+    """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validation(self, name, value):
+        """validate input
+        Args:
+            name (str): asumed always a string
+            vakue (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
