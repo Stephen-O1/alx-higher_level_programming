41ca942c49e96755d55f4719bba6eb343c30cc84
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-09 01:56:08 -0700

    Updated py code style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -13,8 +13,8 @@ def is_same_class(obj, a_class):
          use type() to get specific class
          use isisntance() to get class and any parent class too
          use issubclass() to get what object is a subclass of
-    
+
    Return:
          True if obj is exactly an instance of specified class
     """
-    return type(obj) == a_class
+    return isinstance(obj, a_class)
