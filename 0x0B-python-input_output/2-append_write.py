298c19a9c7f1e832ac31345532b11be36d114004
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-17 10:15:53 +0300

    Correct code for append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ Contains functions that appends to text file and returns num chars added
 """
 
 
-def append_write(filename="", text+""):
+def append_write(filename="", text=""):
     """appends to text file and returns num char added"""
     with open(filename, mode="a", encoding="utf-8") as f:
         return(f.write(text))
