69d146510675d63ff01f5582c85c3970bb370672
Stephen-O1 <onyangostepen654@gmail.com>
2024-04-23 17:55:09 +0300

    Corrected the code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 2) {
 	console.log('No argument');
 } else if (process.argv.lenght === 3) {
-	console.log('Argument found'):
-} else {
 	console.log('Argument found');
+} else {
+	console.log('Arguments found');
 }
