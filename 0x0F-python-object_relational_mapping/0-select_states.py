6184052463ffd177e63c8fdaf3a35c8e56accc7d
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-28 00:04:32 -0700

    script that lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MYSQLdb.connect(host="localhost",
+                          port=3306,
+                          user=argv[1],
+                          passwd=qrgv[2],
+                          db=argv[3])
+
+    # create cursor to exec queries using sql
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
