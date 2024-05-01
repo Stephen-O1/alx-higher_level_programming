2aae39ddcab42f8451c9284753f6076fefe74d4d
Stephen-O1 <onyangostephen654@gmail.com>
2024-04-30 19:11:19 -0700

    Updated files

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Define Class Sate
+Defines class State
 """
 
-
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.exit.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
