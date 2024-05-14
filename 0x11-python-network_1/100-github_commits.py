3edcd1515dbfba782e7ffc45b55fc8b72390e132
Stephen-O1 <onyangostepen654@gmail.com>
2024-05-14 11:50:04 +0300

    Updated script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,16 +3,15 @@
 given repo and owner name as argvs, use Github API to list last 10 commits
 usage: ./100-github_commits.py [github_repo] [github_owner]
 """
-
 from sys import argv
 import requests
 
 
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
-            argv[2], argv[1])
-    r = requets.get(url)
+        argv[2], argv[1])
+    r = requests.get(url)
     list_commits = r.json()
-    for commit on list_commits[0:10]:
-        print(commit.get('sha'). end=': ')
+    for commit in list_commits[0:10]:
+        print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
